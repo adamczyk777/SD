4ba94e88969a12fb7a4fac693f5cbe3f1bc4dc38
Jakub Adamczyk <adamczyk777@gmail.com>
2017-11-07 01:03:54 +0100

    add lab3 files

---
+++ lab3/Jakub_Adamczyk_Skrypt_laboratorium_3.m
@@ -0,0 +1,22 @@
+%Jakub Adamczyk
+
+%czyszczenie przestrzeni nazw i zmiennych
+clear;
+%Zadanie 1:
+%lambde wprowadzamy w konsoli
+lambda=input('lambda = ');
+% Ustawiam WPC
+x(1)=1;
+% uzupelniam wartosci kolejnych x(i) okreslonych ponizszym wzorem
+% rekurencyjnym
+for i=2:20
+    x(i)=lambda*x(i-1);
+end
+%hold on zeby punkty narysowaly sie na jednym figure
+hold on;
+%ustawiam tytu³a dla naszego wykresu
+title=strcat('lambda = ', num2str(lambda),'  WPC = ', num2str(x(1)));
+%rysuje wykres
+plot(x,'*r');
+%mozemy zrobic hold off po narysowaniu
+hold off;
