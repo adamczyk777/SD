a673524d7b2fa5babf1569f8c50f7568ddf7c75a
Jakub Adamczyk <adamczyk777@users.noreply.github.com>
2017-11-20 09:30:08 +0100

    Update Jakub_Adamczyk_Skrypt_laboratorium_3.m

--- lab3/Jakub_Adamczyk_Skrypt_laboratorium_3.m
+++ lab3/Jakub_Adamczyk_Skrypt_laboratorium_3.m
@@ -1,24 +1,54 @@
 %Jakub Adamczyk
-
-%czyszczenie przestrzeni nazw i zmiennych
 clear;
+
+%wybieranie zadania:
+zad = input('podaj numer zadania do wykonania zad = ');
+
 %Zadanie 1:
-%lambde wprowadzamy w konsoli
-lambda=input('lambda = ');
-% Ustawiam WPC
-x(1)=1;
-% uzupelniam wartosci kolejnych x(i) okreslonych ponizszym wzorem
-% rekurencyjnym
-for i=2:20
-    x(i)=lambda*x(i-1);
+if zad == 1
+    %Nasze WPC czyli x(1)
+    x(1)=input('podaj WPC x(1) = ');
+
+    %Wprowadzamy lambde:
+    lambda = input('podaj lambde, lambda = ');
+
+    % uzupelniam wartosci kolejnych x(i) okreslonych ponizszym wzorem
+    % rekurencyjnym
+    for i=2:20
+        x(i)=lambda*x(i-1);
+    end
+    %tworze nowe okno na wykres
+    figure;
+    %hold on zeby punkty narysowaly sie na jednym figure
+    hold on;
+    %ustawiam tytuła dla naszego wykresu
+    title=strcat('lambda = ', num2str(lambda),'  WPC = ', num2str(x(1)));
+    %rysuje wykres
+    plot(x,'*r');
+    %mozemy zrobic hold off po narysowaniu
+    hold off;
+
+elseif zad == 2
+    %Zadanie 2:
+    clear;
+
+elseif zad == 3
+    %Zadanie 3:
+    clear;
+    %kwota początkowa
+    K_0 = input('kwota poczatkowa = ');
+    r = input('stopa procentowa w skali roku = ');
+    n = input('ile lat pieniadze s ana lokacie n = ');
+    for m=1:12
+        %Wzór na kwote po n lat dla r stopy i m kapitalizacji w roku
+        K(m)=K_0*(1+r/m)^(m*n);
+    end 
+    %rysowanie wykresu
+    figure;
+    hold on;
+    title = strcat('kwota zgromadzona po ', num2str(n),' latach, przy oprocentowaniu ', num2str(r), '%, oraz kapitalizacji ', num2str(m), ' razy w roku'); 
+    plot(K,'*r');
+    hold off;
+else
+    %Zadanie 4:
 end
-%tworze nowe okno na wykres
-figure;
-%hold on zeby punkty narysowaly sie na jednym figure
-hold on;
-%ustawiam tytu�a dla naszego wykresu
-title=strcat('lambda = ', num2str(lambda),'  WPC = ', num2str(x(1)));
-%rysuje wykres
-plot(x,'*r');
-%mozemy zrobic hold off po narysowaniu
-hold off;
