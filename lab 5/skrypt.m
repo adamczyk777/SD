dc8bb6d4db4fee2c7bee82ec24b6c5b89a201290
Jakub Adamczyk <jkk.adamczyk@gmail.com>
2017-12-13 20:44:11 +0100

    lab5

---
+++ lab 5/skrypt.m
@@ -0,0 +1,32 @@
+%Jakub Adamczyk
+
+clear;
+
+% menu do wyboru danego zadania
+choice = menu('Wybierz zadanie: ', 'Stabilno?? punktów równowagi', 'Wp?yw warto?ci epsilon na rozmiar otoczenia', 'Wp?yw nie spe?nienia za?o?e? twierdzenia 2', 'Dzia?anie twierdzenia 2');
+
+% Switch z odpowiednimi numerami zadania:
+switch choice
+    case 1
+        clear;
+        % Zbada? i zilustrowa? wykresami w dziedzine czasu (dla systemu z praw? stron? postaci
+        % (1.2) ) i portretami fazowymi (dla systemów z prawymi stronami (1.3) i (1.4) ) stabilno??
+        % (lub niestabilno??) punktów równowagi;
+        
+        
+    case 2
+        % Na podstawie przyk?adu (1.2) zanalizowa? wp?yw warto?ci ? na rozmiar otoczenia, w
+        % którym dynamiki systemu zlienaryzowanego i nieliniowego s? podobne;
+        
+        
+    case 3
+        %  Na podstawie przyk?adu (1.3) zweryfikowa? wp?yw nie spe?nienia za?o?e? twierdzenia 2;
+        
+        
+    case 4
+        %  Na podstawie przyk?adu 3 pokaza? dzia?anie twierdzenia 2 - porównuj?c portret fazowy
+        % systemu nieliniowego i jego linearyzacji w obydwu punktach równowagi.
+        
+        
+end
+
