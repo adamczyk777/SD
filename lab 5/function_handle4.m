c14618f79675ccd60b132b51bff76e662322c875
Jakub Adamczyk <adamczyk777@gmail.com>
2017-12-14 22:27:59 +0100

    new

---
+++ lab 5/function_handle4.m
@@ -0,0 +1,9 @@
+function result = function_handle4( t, x )
+%FUNCTION_HANDLE4 Summary of this function goes here
+%   Detailed explanation goes here
+    result = zeros(size(x));
+    result(1)=-x(1);
+    result(2)=0;
+
+end
+
---
+++ lab 5/function_handle5.m
@@ -0,0 +1,9 @@
+function result = function_handle5( t, x )
+%FUNCTION_HANDLE5 Summary of this function goes here
+%   Detailed explanation goes here
+    result = zeros(size(x));
+    result(1) = x(2);
+    result(2) = 2*x(1)-x(2);
+
+end
+
