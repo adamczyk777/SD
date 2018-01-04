063fd43d564377adb9445f9fe1d5b2bbd509c7ca
Jakub Adamczyk <jkk.adamczyk@gmail.com>
2018-01-04 20:25:03 +0100

    update

---
+++ lab5/function_handle1.m
@@ -0,0 +1,8 @@
+%Jakub Adamczyk
+
+function y = function_handle1( t, x, epsilon )
+%FUNCTION_HANDLE1 Summary of this function goes here
+%   Detailed explanation goes here
+  y=-x+epsilon*x^2;
+end
+
